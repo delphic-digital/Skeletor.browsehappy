b02a18d296e19d5d48e6c3654ed7662a7c084f00
Jeff <jmills@delphicdigital.com>
2016-06-16 15:36:06 -0400

    Use new skeletor plugin factory

--- skeletor.browsehappy.js
+++ skeletor.browsehappy.js
@@ -7,43 +7,36 @@
 
 define(['jquery', 'skeletor.core'],function ($, Skeletor){
 
-	var BrowseHappy = function(element, options){
-		this.options = $.extend({}, this.defaults, options);
-		this.$element = element || $(document);
-		this._init();
-		Skeletor.instantiatePlugin(this);
-	};
+	function BrowseHappy(element, options) {
+		BrowseHappy.__super__.call(this, element, options, BrowseHappy.DEFAULTS);
+	}
 
-	BrowseHappy.prototype = {
-		name: 'BrowseHappy',
-		version: '1.0.0',
-		defaults: {
-			vs: {i:9,f:25,o:12.1,s:2,c:10}, // browser versions to notify
-			reminder: 24,                   // after how many hours should the message reappear
-			                                // 0 = show all the time
-			reminderClosed: 150,            // if the user closes message it reappears after x hours
-			onshow: function(infos){},      // callback function after the bar has appeared
-			onclick: function(infos){},     // callback function if bar was clicked
-			onclose: function(infos){},     // callback function after the bar is closed
-			l: false,                       // set a language for the message, e.g. "en"
-			                                // overrides the default detection
-			test: false,                    // true = always show the bar (for testing)
-			text: "",                       // custom notification html text
-			                                // Optionally include up to two placeholders "%s" which will be replaced with the browser version and contents of the link tag. Example: "Your browser (%s) is old.  Please <a%s>update</a>"
-			text_xx: "",                    // custom notification text for language "xx"
-			                                // e.g. text_de for german and text_it for italian
-			newwindow: true,                // open link in new window/tab
-			url: "http://browsehappy.com/"
-		},
-		constructor: BrowseHappy,
-		_init: function(){
+	BrowseHappy.VERSION = '0.0.1';
+	BrowseHappy.DEFAULTS =  {
+		vs: {i:9,f:25,o:12.1,s:2,c:10}, // browser versions to notify
+		reminder: 24,                   // after how many hours should the message reappear
+		                                // 0 = show all the time
+		reminderClosed: 150,            // if the user closes message it reappears after x hours
+		onshow: function(infos){},      // callback function after the bar has appeared
+		onclick: function(infos){},     // callback function if bar was clicked
+		onclose: function(infos){},     // callback function after the bar is closed
+		l: false,                       // set a language for the message, e.g. "en"
+		                                // overrides the default detection
+		test: false,                    // true = always show the bar (for testing)
+		text: "",                       // custom notification html text
+		                                // Optionally include up to two placeholders "%s" which will be replaced with the browser version and contents of the link tag. Example: "Your browser (%s) is old.  Please <a%s>update</a>"
+		text_xx: "",                    // custom notification text for language "xx"
+		                                // e.g. text_de for german and text_it for italian
+		newwindow: true,                // open link in new window/tab
+		url: "http://browsehappy.com/"
+	}
+
+	Skeletor.Plugin.create(BrowseHappy, {
+		_init: function(element) {
 			window.$buoop = this.options;
 			require(['//browser-update.org/update.min.js'])
 		}
-	}
+	});
 
-	Skeletor.registerPlugin(BrowseHappy);
-
-	new BrowseHappy();
-
+	new BrowseHappy()
 });
